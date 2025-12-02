9b80faab53f252d8ceeb6356ee28a172cc84198f
Nik Varga <ndv2003.ndv@gmail.com>
2025-12-02 12:41:23 -0500

    add parameters outline

---
+++ parameters.h
@@ -0,0 +1,36 @@
+#pragma once
+
+enum ControlParameters {
+  // C_frequency, detune, level, octave, waveshape (button)
+  OSC_1_PARAMS = 0,
+  
+  OSC_1_ADSR,
+  // Detune, level, semitone offset, waveshape (button)
+  OSC_2_PARAMS,
+  
+  OSC_2_ADSR,
+
+  // intensity, nosie enable (button)
+  ADDITIVE_NOISE,
+
+  // C_freq, Q, filter type (button)
+  FILTER_PARAMS,
+
+  // Waveform, rate, intensity, DC offset. Mapping through GUI
+  LFO_PARAMS,
+
+  // delay length, feedback, delay enable (button)
+  DELAY_PARAMS,
+  
+  //intensity, frequency, waveshape (button)
+  AM_PARAMS,
+
+  //intensity, frequency, waveshape (button)
+  FM_PARAMS,
+
+  ARP,
+
+  GLOBAL_CONTROLS
+};
+
+// TODO: decide whether to use a class
