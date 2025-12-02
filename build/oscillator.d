82d48faeac91afbfb22679fce1121e9b7d2eb22e
Nik Varga <ndv2003.ndv@gmail.com>
2025-12-02 11:48:27 -0500

    Refactor oscillator implementation to support multiple harmonics and improve performance
    - Updated Oscillator constructor to accept float sample rate instead of unsigned int.
    - Modified setup method to include nHarmonics parameter and initialize wavetables accordingly.
    - Enhanced waveshape generation logic for square, saw, sine, and triangle waves.
    - Implemented fundamental frequency setting with anti-aliasing for harmonics.
    - Replaced Wavetable instances with Oscillator in render.cpp for improved functionality.
    - Added debounce logic for oscillator type switching via button press.
    - Cleaned up wavetable processing and amplitude setting for better clarity and performance.

---
+++ build/oscillator.d
@@ -0,0 +1,8 @@
+/root/Bela/projects/tabbasar/build/oscillator.o: \
+  /root/Bela/projects/tabbasar/oscillator.cpp \
+  /root/Bela/projects/tabbasar/oscillator.h \
+  /root/Bela/projects/tabbasar/wavetable.h
+
+/root/Bela/projects/tabbasar/oscillator.h:
+
+/root/Bela/projects/tabbasar/wavetable.h:
--- build/render.d
+++ build/render.d
@@ -2,7 +2,13 @@
   /root/Bela/projects/tabbasar/render.cpp include/Bela.h \
   /usr/xenomai/include/cobalt/unistd.h \
   /usr/xenomai/include/cobalt/stdio.h include/GPIOcontrol.h \
-  include/Utilities.h
+  include/Utilities.h libraries/Scope/Scope.h include/AuxTaskNonRT.h \
+  /usr/xenomai/include/cobalt/pthread.h include/AuxTaskRT.h \
+  /usr/xenomai/include/cobalt/fcntl.h \
+  /usr/xenomai/include/cobalt/mqueue.h libraries/Biquad/Biquad.h \
+  /root/Bela/projects/tabbasar/Filter.h \
+  /root/Bela/projects/tabbasar/oscillator.h \
+  /root/Bela/projects/tabbasar/wavetable.h
 
 include/Bela.h:
 
@@ -13,3 +19,23 @@ include/Bela.h:
 include/GPIOcontrol.h:
 
 include/Utilities.h:
+
+libraries/Scope/Scope.h:
+
+include/AuxTaskNonRT.h:
+
+/usr/xenomai/include/cobalt/pthread.h:
+
+include/AuxTaskRT.h:
+
+/usr/xenomai/include/cobalt/fcntl.h:
+
+/usr/xenomai/include/cobalt/mqueue.h:
+
+libraries/Biquad/Biquad.h:
+
+/root/Bela/projects/tabbasar/Filter.h:
+
+/root/Bela/projects/tabbasar/oscillator.h:
+
+/root/Bela/projects/tabbasar/wavetable.h:
