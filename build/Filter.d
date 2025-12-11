b8ed9f5f53ad0c0e400983bcc8ef901e48ed2156
Nik Varga <ndv2003.ndv@gmail.com>
2025-12-10 23:59:47 -0500

    remove build folder from git tracking

--- build/Filter.d
+++
@@ -1,5 +0,0 @@
-/root/Bela/projects/tabbasar/build/Filter.o: \
-  /root/Bela/projects/tabbasar/Filter.cpp \
-  /root/Bela/projects/tabbasar/Filter.h
-
-/root/Bela/projects/tabbasar/Filter.h:
--- build/oscillator.d
+++
@@ -1,8 +0,0 @@
-/root/Bela/projects/tabbasar/build/oscillator.o: \
-  /root/Bela/projects/tabbasar/oscillator.cpp \
-  /root/Bela/projects/tabbasar/oscillator.h \
-  /root/Bela/projects/tabbasar/wavetable.h
-
-/root/Bela/projects/tabbasar/oscillator.h:
-
-/root/Bela/projects/tabbasar/wavetable.h:
